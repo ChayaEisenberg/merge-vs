3fd92d7c1f75bb72e6da43ce44308f48b2445d10
אייזנברג חיה < @mbjcomp.local>
2023-12-19 20:51:26 +0200

    update project2

--- merge-vs/merge-vs/Program.cs
+++ merge-vs/merge-vs/Program.cs
@@ -1,8 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
 Console.WriteLine("Lefdgbgbgfbeba");
-=======
-Console.WriteLine("chaya");
->>>>>>> 28f2fdd8ce5e86d22f5851fa0ee78104bc76140c
 
